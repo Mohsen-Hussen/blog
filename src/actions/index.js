15dac7f0741483fb52db07977ad8ea726e0dc871
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-12-08 15:26:36 +0200

    add user header component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,8 @@ export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceHolder.get('/posts');
     dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER' , payload:response.data});
+};
